c9d2102175814eab63f97a73f21d8c048a54c837
Igor Lipodat <lipodat.igor@gmail.com>
2024-07-28 15:49:45 +0200

    Added timer to web for refreshing every 10 seconds

--- CryptoExchange.WebClient/Pages/Home.razor.cs
+++ CryptoExchange.WebClient/Pages/Home.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using System.Net.Http.Json;
 using System.Net.Http;
+using System.Timers;
 
 namespace CryptoExchange.WebClient.Pages;
 
@@ -9,8 +10,27 @@ public partial class Home
 {
     [Inject] private HttpClient _httpClient { get; set; } = default!;
     private OrderBookRecord _orderBook = new();
+    private System.Timers.Timer _timer;
+    public Home()
+    {
+        _timer = new()
+        {
+            Interval = 10000
+        };
+        _timer.Elapsed += async (object? sender, ElapsedEventArgs e) =>
+        {
+            await InvokeAsync(UpdateOrderBookAsync);
+        };
+        _timer.Enabled = true;
+    }
     protected override async Task OnInitializedAsync()
     {
+        await UpdateOrderBookAsync();
+    }
+
+    private async Task UpdateOrderBookAsync()
+    {
         _orderBook = await _httpClient.GetFromJsonAsync<OrderBookRecord?>("OrderBook") ?? new();
+        StateHasChanged();
     }
 }
